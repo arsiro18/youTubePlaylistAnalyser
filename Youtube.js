989ddca39595838023bf1d1d4d30851970cadb14
arsiro18 <arsiro8@gmail.com>
2023-05-25 23:59:48 +0530

    Web Scrapping

--- Youtube.js
+++ Youtube.js
@@ -1,18 +1,35 @@
 const puppeteer = require('puppeteer')
 
 let cTab
+let link = 'https://www.youtube.com/playlist?list=PLW-S5oymMexXTgRyT3BWVt_y608nt85Uj'
 
-(async function(){
+async function call(){
     try{
         let broswerOpen = puppeteer.launch({
             headless: false,
             defaultViewport:null,
             args:['start-maximised']
         })
-        let broswerInstnce = await broswerOpen
-        let allTabsArr = await broswerInstnce.pages()
+        let broswerInstance = await broswerOpen
+        let allTabsArr = await broswerInstance.pages()
         cTab = allTabsArr[0]
+        await cTab.goto(link)
+        await cTab.waitForSelector('.style-scope.yt-dynamic-sizing-formatted-string.yt-sans-28')
+        let name = await cTab.evaluate(function(select){return document.querySelector(select).innerText},'.style-scope.yt-dynamic-sizing-formatted-string.yt-sans-28')
+        let allData = await cTab.evaluate(getData,'.byline-item.style-scope.ytd-playlist-byline-renderer')
+        console.log(name,allData.noOfVideos,allData.noOfViews)
     }catch(error){
 
     }
-})()
+}
+
+function getData(selector){
+    let allElements = document.querySelectorAll(selector)
+    let noOfVideos = allElements[0].innerText
+    let noOfViews = allElements[1].innerText
+    return {
+        noOfVideos,
+        noOfViews
+    }
+}
+call()
