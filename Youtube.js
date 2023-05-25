0da957936859977f734a8b9230898f3ab7b82cee
arsiro18 <arsiro8@gmail.com>
2023-05-26 01:00:06 +0530

    Data Analysation Completed

--- Youtube.js
+++ Youtube.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer')
+const pdf =require('pdfkit')
+const fs = require('fs')
 
 let cTab
 let link = 'https://www.youtube.com/playlist?list=PLW-S5oymMexXTgRyT3BWVt_y608nt85Uj'
@@ -18,8 +20,25 @@ async function call(){
         let name = await cTab.evaluate(function(select){return document.querySelector(select).innerText},'.style-scope.yt-dynamic-sizing-formatted-string.yt-sans-28')
         let allData = await cTab.evaluate(getData,'.byline-item.style-scope.ytd-playlist-byline-renderer')
         console.log(name,allData.noOfVideos,allData.noOfViews)
-    }catch(error){
 
+        let totalVideos = allData.noOfVideos.split(" ")[0]
+        console.log(totalVideos)
+
+        let currentVideos = await getCvideosLen()
+        console.log(currentVideos)
+
+        while(totalVideos-currentVideos>=20){
+            await scrollToBottom()
+            currentVideos = await getCvideosLen()
+        }
+        let finalList = await getStats ()
+        let pdfDoc = new pdf
+        pdfDoc.pipe(fs.createWriteStream('playlist.pdf'))
+        pdfDoc.text(JSON.stringify(finalList))
+        pdfDoc.end()
+
+    }catch(error){
+        console.log(error)
     }
 }
 
@@ -31,5 +50,38 @@ function getData(selector){
         noOfVideos,
         noOfViews
     }
+}
+
+async function getCvideosLen(){
+    let length = await cTab.evaluate(getLength , '#container>#thumbnail span.style-scope.ytd-thumbnail-overlay-time-status-renderer')
+    return length
+}
+
+async function scrollToBottom(){
+    await cTab.evaluate(goToBottom)
+    function goToBottom(){
+        window.scrollBy(0,window.innerHeight)
+    }
+}
+async function getStats(){
+    let list = cTab.evaluate(getNameAndDuration,'#video-title','#container>#thumbnail span.style-scope.ytd-thumbnail-overlay-time-status-renderer')
+    return list
+}
+function getLength(durationSelect){
+    let durationEle = document.querySelectorAll(durationSelect)
+    return durationEle.length
+}
+
+function getNameAndDuration(videoSelector,durationSelector){
+    let videoElem = document.querySelectorAll(videoSelector)
+    let durationElem = document.querySelectorAll(durationSelector)
+
+    let currList = []
+    for(let i=0;i<durationElem.length;i++){
+        let videoTitle = videoElem[i].innerText
+        let duration = durationElem[i].innerText
+        currList.push({videoTitle,duration})
+    }
+    return currList;
 }
 call()
